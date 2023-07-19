0843616587de3ab779cea46d89a2281ac76fd948
shahriyor-sharifjonov <shahrizodadeveloping@gmail.com>
2023-07-19 19:19:36 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,13 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const menu = document.querySelector('.menu');
+const list = document.querySelector('.header__bottom.enable-sm');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    menu.classList.toggle('active')
+    list.classList.toggle('active')
+    body.classList.toggle('active')
+})
